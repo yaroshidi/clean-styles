0895ef9e878748d5fb46c01cb88febc3c4eef251
yaroshidi <yaroshidi@hotmail.com>
2025-01-12 16:38:11 +0100

    initial setup complete

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  console.log(webflow);
+</script>
+
+<main>
+  <h1>Clean Styles</h1>
+  <button>this is a button</button>
+</main>
+
+<style>
+</style>
---
+++ src/types/webflow.d.ts
@@ -0,0 +1,5 @@
+import type { Webflow } from '@webflow/designer-extension-typings';
+
+declare global {
+    const webflow: Webflow;
+} 
